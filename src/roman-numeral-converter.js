d007c455a96d94466ae73f6480a4481eba701f8d
Jordan Cooper <jordan@cooperplanet.com>
2019-09-24 11:20:47 -0600

    Update test to import src file

---
+++ src/roman-numeral-converter.js
@@ -0,0 +1,7 @@
+const convertToRoman = number => {
+  return number
+}
+
+module.exports = {
+  convertToRoman,
+}
--- src/roman-numeral-converter.specs.js
+++ src/roman-numeral-converter.specs.js
@@ -1,3 +1,5 @@
+const { convertToRoman } = require('./roman-numeral-converter')
+
 context('when running the test', () => {
   it('should pass', () => {
     expect(true).to.be.true
